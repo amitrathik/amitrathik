12ad175ec5da6717a5d4741936ebf6a1226fa2bf
Amit Rathi <amit.rathi.k@gmail.com>
2023-08-17 09:44:08 -0400

    overrode styles to make background full width and height for now

--- website/src/views/about/profile/list.js
+++ website/src/views/about/profile/list.js
@@ -2,11 +2,11 @@ const ProfileList = () => {
     return `<ul class="profile-list">
     <li class="clearfix">
         <strong class="title">E-mail</strong>
-        <span class="cont"><a href="mailto:robertsmith@company.com">robertsmith@company.com</a></span>
+        <span class="cont"><a href="mailto:amit@themobidev.com">amit@themobidev.com</a></span>
     </li>
     <li class="clearfix">
         <strong class="title">Phone</strong>
-        <span class="cont"><a href="tel:+12562548456">+1 256 254 84 56</a></span>
+        <span class="cont"><a href="tel:+3132889917">+1 313 288 9917</a></span>
     </li>
 </ul>`
 }
